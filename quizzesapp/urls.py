a20d87a67bc04dc59b641c28426e13a5f4299ea4
Kno777 <kno0109@mail.ru>
2022-11-18 11:42:39 +0400

    added three new URL paths for get quiz_list, quiz_detail and share quizzes also added app_name variable

--- quizzesapp/urls.py
+++ quizzesapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import ListQuiz
+from . import views
+
+app_name = 'quizzesapp'
 
 urlpatterns = [
-    path('quizzes/', ListQuiz.as_view(), name=ListQuiz.name)
+    path('quiz/', views.quiz_list, name='quiz_list'),
+    #path('quiz/', views.PostListView.as_view(), name='quiz_list'),
+    path('quiz/<int:id>/', views.quiz_detail, name='quiz_detail'),
+    path('quiz/<int:quiz_id>/share/', views.quiz_share, name='quiz_share'),
 ]
