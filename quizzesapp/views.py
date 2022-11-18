fd3cd12490b45eb3894e075a68c6aafd2d272103
Kno777 <kno0109@mail.ru>
2022-11-18 11:45:08 +0400

    added three function those are <quiz_list>, <quiz_detail> and <quiz_share>

--- quizzesapp/views.py
+++ quizzesapp/views.py
@@ -1,41 +1,63 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import QuizzesPython, Quizzes_Users_Answers
 from django.contrib.auth.models import User
-from rest_framework import generics
-from rest_framework import filters
-from rest_framework import pagination
-from rest_framework.permissions import IsAdminUser, AllowAny
-from .serializers import QuizzesPythonSerializer
+from django.contrib.auth import get_user_model
+from django.views.generic import ListView
+from django.core.mail import send_mail
+from .forms import EmailPostForm
+from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# def index(request):
-#     quiz = QuizzesPython.objects.all()
-#     us = Quizzes_Users_Answers.objects.all()
-#     context = {"quiz":quiz, "us":us}
-#     v = us.get(id=request.user.id)
-#     ans = 'dfdfdf'
-#     if request.user.is_authenticated:
-#         if request.method == "POST":
-#             ans = request.POST.get('answer')
-#             v.answer = ans
-#             v.save()
-#             return HttpResponse(ans)
-#     return render(request, 'registration/index.html', context)
 
-class LargeResultsSetPagination(pagination.PageNumberPagination):
-    page_size = 5
-    page_size_query_param = 'page_size'
-    max_page_size = 1000
+def quiz_list(request):
+  post_list = QuizzesPython.objects.all()
 
-class StandardResultsSetPagination(pagination.PageNumberPagination):
-    page_size = 5
-    page_size_query_param = 'page_size'
-    max_page_size = 1000
+  paginator = Paginator(post_list, 3) 
+  page_number = request.GET.get('page', 1) 
+  try:
+    posts = paginator.page(page_number)
+  except PageNotAnInteger:
+    # If page_number is not an integer deliver the first page 
+    posts = paginator.page(1)
+  except EmptyPage:
+    # If page_number is out of range deliver last page of results 
+    posts = paginator.page(paginator.num_pages)
+  
+  return render(request,'quizzesapp/quizzes/list.html', {'posts': posts})
 
-class ListQuiz(generics.ListAPIView): 
-    queryset = QuizzesPython.objects.all() 
-    serializer_class = QuizzesPythonSerializer
-    permission_classes = [AllowAny]
-    pagination_class = LargeResultsSetPagination
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['section', 'question', 'id']
-    name = 'List Quiz'
+# class PostListView(ListView):
+#   """
+#   Alternative post list view
+#   """
+#   queryset = QuizzesPython.objects.all()
+#   context_object_name = 'posts'
+#   paginate_by = 3
+#   template_name = 'quizzesapp/quizzes/list.html'
+
+def quiz_detail(request, id):
+  post = get_object_or_404(QuizzesPython, id=id)
+  ans = Quizzes_Users_Answers.objects.all()
+  
+  return render(request,'quizzesapp/quizzes/detail.html',{'post': post})
+
+
+def quiz_share(request, quiz_id):
+  # Retrieve post by id
+  post = get_object_or_404(QuizzesPython, id=quiz_id) 
+  sent = False
+
+  if request.method == 'POST':
+    # Form was submitted
+    form = EmailPostForm(request.POST)
+    if form.is_valid():
+      # Form fields passed validation
+      cd = form.cleaned_data
+      # ... send email
+      post_url = request.build_absolute_uri(post.get_absolute_url())
+      subject = f"{cd['name']} recommends you read " f"{post.question}"
+      message = f"Read {post.question} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
+      send_mail(subject, message, 'knyazharutyunyan03@gmail.com', [cd['to']])
+      sent = True
+  else:
+        form = EmailPostForm()
+  return render(request, 'quizzesapp/quizzes/share.html', {'post': post, 'form': form, 'sent': sent})
