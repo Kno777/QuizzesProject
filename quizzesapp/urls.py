123e1194b2e4a7fa585febe805c16800f9632ffe
Kno777 <kno0109@mail.ru>
2022-11-23 18:44:24 +0400

    added URL path for searching

--- quizzesapp/urls.py
+++ quizzesapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     #path('quiz/', views.PostListView.as_view(), name='quiz_list'),
     path('quiz/<int:id>/', views.quiz_detail, name='quiz_detail'),
     path('quiz/<int:quiz_id>/share/', views.quiz_share, name='quiz_share'),
+    path('search/', views.post_search, name='post_search'),
 ]
