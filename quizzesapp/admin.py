84ee9e6b1932c157941079c427ba9b19c149859b
Kno777 <kno0109@mail.ru>
2022-12-04 00:51:05 +0400

    added filter, search and display panel for Admin Page

--- quizzesapp/admin.py
+++ quizzesapp/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Quizzes_Users_Answers, QuizzesPython
 
-admin.site.register(QuizzesPython)
-admin.site.register(Quizzes_Users_Answers)
+
+@admin.register(QuizzesPython)
+class AdminQuizzesPython(admin.ModelAdmin):
+    list_display = ['question', 'section', 'admin_correct_answer', 'id',] 
+    list_filter = ['question', 'section', 'id'] 
+    search_fields = ['question', 'section', 'solution']
+
+@admin.register(Quizzes_Users_Answers)
+class AdminQuizzes_Users_Answers(admin.ModelAdmin):
+    list_display = ['user_id', 'quiz_id', 'answer', 'id',] 
+    list_filter = ['user_id', 'quiz_id', 'id'] 
+    search_fields = ['user_id', 'quiz_id', 'answer']
