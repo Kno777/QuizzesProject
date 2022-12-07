b07fea4d7b22d8dc814a7fe7f2531eb66a83a1e4
Kno777 <kno0109@mail.ru>
2022-12-07 23:11:06 +0400

    testing views

--- quizzesapp/urls.py
+++ quizzesapp/urls.py
@@ -6,7 +6,7 @@ app_name = 'quizzesapp'
 urlpatterns = [
     path('quiz/', views.quiz_list, name='quiz_list'),
     #path('quiz/', views.PostListView.as_view(), name='quiz_list'),
-    path('quiz/<int:id>/', views.quiz_detail, name='quiz_detail'),
-    path('quiz/<int:quiz_id>/share/', views.quiz_share, name='quiz_share'),
-    path('search/', views.post_search, name='post_search'),
+    path('quiz/question/<int:id>/', views.quiz_detail, name='quiz_detail'),
+    path('quiz/question/<int:quiz_id>/share/', views.quiz_share, name='quiz_share'),
+    path('quiz/search/', views.post_search, name='post_search'),
 ]
