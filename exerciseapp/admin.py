dcd953e7ebd691d174c89368b623b6c814d1d96f
Kno777 <kno0109@mail.ru>
2022-12-13 23:13:32 +0400

    added exercises admin panel

--- exerciseapp/admin.py
+++ exerciseapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Exercises, Exercises_Users_Answers
 
-# Register your models here.
+
+@admin.register(Exercises)
+class AdminExercises(admin.ModelAdmin):
+    list_display = ['title', 'difficulty', 'admin_correct_answer', 'id',] 
+    list_filter = ['title', 'difficulty', 'id'] 
+    search_fields = ['title', 'difficulty', 'id']
+
+@admin.register(Exercises_Users_Answers)
+class AdminExercises_Users_Answers(admin.ModelAdmin):
+    list_display = ['user_id', 'exercise_id', 'answer', 'id',] 
+    list_filter = ['user_id', 'exercise_id', 'id'] 
+    search_fields = ['user_id', 'exercise_id', 'answer']
