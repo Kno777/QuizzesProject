566223cbe8285b49aef7bf3c03c41e3cba42be93
Kno777 <kno0109@mail.ru>
2022-12-22 20:51:22 +0400

    added two classes for Own app forms

--- ownapp/forms.py
+++ ownapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import OwnQuizzesOfUsersPython, OwnExerciesesOfUsers
 
 DIFFICULTY_CHOOSE = (
     ('Easy', 'Easy'),
@@ -8,17 +9,30 @@ DIFFICULTY_CHOOSE = (
 )
 
 class OwnQuizzesForm(forms.Form):
-    code_quiz = forms.TextInput()
+    code_quiz = forms.CharField(widget=forms.Textarea)
     result = forms.CharField(max_length=255)
-    hint = forms.TextInput(blank=True)
-    solution = forms.TextInput(blank=True)
+    hint = forms.CharField(widget=forms.Textarea)
+    solution = forms.CharField(widget=forms.Textarea)
     email = forms.EmailField()
-    difficulty = forms.CharField(max_length=50, choices=DIFFICULTY_CHOOSE)
+    difficulty = forms.ChoiceField(choices=DIFFICULTY_CHOOSE)
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if OwnQuizzesOfUsersPython.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+
 
 class OwnExercisesForm(forms.Form):
     title = forms.CharField(max_length=350)
-    description = forms.TextInput()
+    description = forms.CharField(widget=forms.Textarea)
     result = forms.CharField(max_length=300)
-    difficulty = forms.CharField(max_length=50, choices=DIFFICULTY_CHOOSE)
-    code_for_example = forms.TextInput()
+    code_for_example = forms.CharField(widget=forms.Textarea)
     email = forms.EmailField()
+    difficulty = forms.ChoiceField(choices=DIFFICULTY_CHOOSE)
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if OwnExerciesesOfUsers.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
