3c63ab7434538f202eab8fc236fc536b1f77d686
Kno777 <kno0109@mail.ru>
2022-12-22 20:52:35 +0400

    added a few URLs paths for ownapp

--- ownapp/urls.py
+++ ownapp/urls.py
@@ -5,4 +5,6 @@ app_name = 'ownapp'
 
 urlpatterns = [
     path('quiz/own/list/', views.own_list, name='own_list'),
+    path('quiz/own/quizzes/', views.own_quiz, name='own_quiz'),
+    path('quiz/own/exercises/', views.own_exercise, name='own_exercise')
 ]
